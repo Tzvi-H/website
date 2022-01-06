661f81810b3167fc9a1b3f0cb9f5631482f2efed
Tzvi-H <16guitar@gmail.com>
2022-01-06 16:51:33 -0500

    update

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-if (__dirname.slice(-6) === '/public') { // For production
-  app.use(express.static(__dirname))
-} else { // For development
-  app.use(express.static(__dirname + '/public'))
-}
-
+app.use(express.static('public'));
+app.use('*', (req, res) => {
+  res.send('hello')
+})
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
