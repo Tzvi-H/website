f7222cadef7225c9051bc64800749844d307e85c
Tzvi-H <16guitar@gmail.com>
2022-01-06 16:50:02 -0500

    added conditional express.static for production

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+if (__dirname.slice(-6) === '/public') { // For production
+  app.use(express.static(__dirname))
+} else { // For development
+  app.use(express.static(__dirname + '/public'))
+}
+
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
